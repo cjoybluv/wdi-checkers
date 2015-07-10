1cea80421d2def1f742fbb4c7fe8e8f2a6148da7
CJOYBLUV <dave_hemmer@hotmail.com>
2015-07-10 12:32:42 -0700

    ArrayMatch: boardSet & isMovable

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@
 
 $(function() {
 
-	var currentBoard = [];  // xy arr's row by col; start = 'init setup'
+	var currentBoardPlayers = [];  // xy arr's row by col; start = 'init setup'
+	var currentBoardMovable = [];
 	var currentPlayer = 'W';
 	var currentDirection = '';  // '+' Down the board, '-' Up, 'K' King Up or Down, '' not set
  	
@@ -28,7 +29,17 @@ $(function() {
 	 	return startBoard;
 	 };
 
-
+	 var setBoardMovable = function() {
+	 	currentBoardMovable = [];
+ 		for (var row = 0; row < 8; row++) {
+	 		currentBoardMovable[row] = [];
+	 		for (var col = 0; col < 8; col++) {
+	 			if (currentBoardPlayers[row][col] !== '') {
+	 				currentBoardMovable[row][col] = pieceIsMovable(row,col);
+	 			}
+	 		}
+	 	}
+	 };
 
 
     var gpIDtoSpotID = function(gpID) {
@@ -37,85 +48,73 @@ $(function() {
     var gpIDtoPlayer = function(gpID) {
     	return gpID[2]
     };
-    var isKing = function(gpID) {
-    	// console.log('isKing',gpID[5]);
-    	return (gpID[5]==='K');
+    var isKing = function(row,col) {
+    	console.log('isKing',currentBoardPlayers[row][col]);
+    	return (currentBoardPlayers[row][col][1]==='K');
     };
-    var setDirection = function(gpID) {
-    	if (isKing(gpID)) {
+    var setDirection = function(row,col) {
+    	if (isKing(row,col)) {
     		return 'K';
-    	} else if (gpIDtoPlayer(gpID)==="B") {
+    	} else if (currentBoardPlayers[row][col]==="B") {
     		return '+';
     	} else {
     		return '-';
     	}
     };
     var spotXY = function(spotID) {
-    	var xy = [];
-    	xy[0] = parseInt(spotID[1]);
-    	xy[1] = parseInt(spotID[3]);
-    	return xy;
+    	return spotID[3]+spotID[4];
     };
     var xyToSpotID = function(xy) {
     	return 'r' + xy[0] + 'c' + xy[1];
     };
-    var setAdj = function(spotID,x,y) {
-    	var adjXY = [];
-    	var xy = spotXY(spotID);
-    	// console.log('setAdj : xy',xy);
-
-    	adjXY[0] = xy[0] + x;
-    	adjXY[1] = xy[1] + y;
-    	// console.log('setAdj : adjXY',adjXY)
-    	if ((adjXY[0]<0||adjXY>7)||adjXY[1]<0||adjXY[1]>7) {
-    		return "offBoard"
-    	} else {
-	    	return xyToSpotID(adjXY);
-    	}
-    };
-    var setAdjSpots =  function(spotID) {
-    	// console.log('setAdjSpots',spotID,'Dir',currentDirection);	
-    	var adjSpots = [];
+    var setAdjSpots =  function(row,col) {
+    	var adjSpots = [];  // array of xy elements
     	switch (currentDirection) {
     		case '':
     			break;
 			case '+':    // black
-				// console.log('setAdjSpots',currentDirection,spotID);
-				adjSpots[0] = setAdj(spotID,1,-1);  // Fwd-Left
-				adjSpots[1] = setAdj(spotID,1,1);	// Fwd-Right
+				adjSpots[0] = (row + 1).toString() + (col - 1).toString();
+				adjSpots[1] = (row + 1).toString() + (col + 1).toString();
 				break;
 			case '-':     // white
-				adjSpots[0] = setAdj(spotID,-1,-1);  // Bwd-Left
-				adjSpots[1] = setAdj(spotID,-1,1);	//Bkwd-Right
+				adjSpots[0] = (row - 1).toString() + (col - 1).toString();
+				adjSpots[1] = (row - 1).toString() + (col + 1).toString();
 				break;
 			case 'K':
-				adjSpots[0] = setAdj(spotID,1,-1);  // Fwd-Left
-				adjSpots[1] = setAdj(spotID,1,1);	// Fwd-Right
-				adjSpots[2] = setAdj(spotID,-1,-1);  // Bkwd-Left
-				adjSpots[3] = setAdj(spotID,-1,1);	// BKwd-Right
+				adjSpots[0] = (row + 1).toString() + (col - 1).toString();
+				adjSpots[1] = (row + 1).toString() + (col + 1).toString();
+				adjSpots[2] = (row - 1).toString() + (col - 1).toString();
+				adjSpots[3] = (row - 1).toString() + (col + 1).toString();
 				break;
     	}
+    	adjSpots.forEach(function(spot) {
+			if ((spot[0]<0||spot>7)||spot[1]<0||spot[1]>7) {
+				return "offBoard"
+			} else {
+				return spot;
+			}
+    	});
     	return adjSpots;
     };
 
 
 	 var displayBoard = function() {
-	 	// console.log(currentBoard[0][2]);
+	 	// console.log(currentBoardPlayers[0][2]);
 	 	for (var row = 0;row < 8;row++) {
 	 		for(var col = 0;col <8;col++) {
 	 			var spotID = xyToSpotID(row.toString() + col.toString());
 	 			// console.log(row.toString() + col.toString());
-	 			switch (currentBoard[row][col]) {
+	 			switch (currentBoardPlayers[row][col]) {
 	 				case '':
 	 					$('#'+spotID).find('img').remove();
 	 					break;
  					case 'B':
 	 					$('#'+spotID).find('img').remove();
-	 					$('#'+spotID).append('<img id="gbB'+row+col+'" class="game-piece draggable" src="black-king.png" width="50">');
+	 					$('#'+spotID).append('<img id="gbB'+row+col+'" class="game-piece draggable" src="black.png" width="50">');
 	 					break;
  					case 'W':
 	 					$('#'+spotID).find('img').remove();
-	 					$('#'+spotID).append('<img id="gbW'+row+col+'" class="game-piece draggable" src="white-king.png" width="50">');
+	 					$('#'+spotID).append('<img id="gbW'+row+col+'" class="game-piece draggable" src="white.png" width="50">');
 	 					break;
  					case 'BK':
 	 					$('#'+spotID).find('img').remove();
@@ -138,28 +137,37 @@ $(function() {
 	//   OR 2) have opponent in ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
 	// 			AND have theJump +2.diag spot available
 	// 
-	var pieceIsMovable = function(gpID) {
+	var pieceIsMovable = function(row,col) {
 		var movable = false;
-		console.log('In pieceIsMovable',gpID,currentPlayer);
-		if (gpIDtoPlayer(gpID) !== currentPlayer) {
-			console.log('wrong player');
-			return false;
-		}
-		currentDirection = setDirection(gpID);
-		console.log('currentDirection',currentDirection);
-		var OrigSpotID = gpIDtoSpotID(gpID);
-		console.log('OrigSpotID',OrigSpotID);
-		var adjSpots = setAdjSpots(OrigSpotID);
+		// if (currentBoardPlayers[row][col] !== currentPlayer) {
+		// 	console.log('wrong player');
+		// 	return false;
+		// }
+		currentDirection = setDirection(row,col);
+		var adjSpots = setAdjSpots(row,col);
 		console.log('adjSpots',adjSpots);
-		var occupied;
-		adjSpots.forEach(function(spot) {
-			if (spot !== 'offBoard') {
-				occupied = $('#'+spot+' img').length;
-				if (!occupied) {
+
+		// var movable = false;
+		// console.log('In pieceIsMovable',gpID,currentPlayer);
+		// if (gpIDtoPlayer(gpID) !== currentPlayer) {
+		// 	console.log('wrong player');
+		// 	return false;
+		// }
+		// currentDirection = setDirection(gpID);
+		// console.log('currentDirection',currentDirection);
+		// var OrigSpotID = gpIDtoSpotID(gpID);
+		// console.log('OrigSpotID',OrigSpotID);
+		// var adjSpots = setAdjSpots(OrigSpotID);
+		// console.log('adjSpots',adjSpots);
+		// var occupied;
+		var movable = false;
+		for (var i = 0;i < adjSpots.length;i++) {
+			if (adjSpots[i] !== 'offBoard') {
+				if (currentBoardPlayers[adjSpots[i][0]][adjSpots[i][1]]==='') {
 					movable = true;
 				}
 			}
-		});
+		}
 		console.log('movable',movable);
 		return movable;
 	};
@@ -174,15 +182,19 @@ $(function() {
 // 
 // MAINLINE
 // 
-	 	currentBoard = initStartBoard();
+	 	currentBoardPlayers = initStartBoard();
+	 	console.log('players',currentBoardPlayers);
+	 	setBoardMovable();
+	 	console.log('movable',currentBoardMovable);
 	 	displayBoard();
 
 
     $( ".game-piece" ).draggable({ 
     	containment: "#game-board", scroll: false, grid: [ 64, 64 ],
     	start: function(event,ui) {
-    		console.log('drag-start',event.target.id);
-    		pieceIsMovable(event.target.id);
+    		var xy = spotXY(event.target.id);
+    		console.log('drag-start',event.target.id,xy);
+    		pieceIsMovable(xy[0],xy[1]);
     	},
     	stop: function(event,ui) {
     		// console.log('drag-stop',event);

c8e33723cb7e7015bbcdb2f109082ec3a2a8176e
CJOYBLUV <dave_hemmer@hotmail.com>
2015-07-12 13:42:02 -0700

    drop touch punch

--- js/script.js
+++ js/script.js
@@ -30,34 +30,6 @@ $(function() {
  	var gameStarted = false;
 
 
-	function touchHandler(event) {
-	    var touch = event.changedTouches[0];
-
-	    var simulatedEvent = document.createEvent("MouseEvent");
-	        simulatedEvent.initMouseEvent({
-	        touchstart: "mousedown",
-	        touchmove: "mousemove",
-	        touchend: "mouseup"
-	    }[event.type], true, true, window, 1,
-	        touch.screenX, touch.screenY,
-	        touch.clientX, touch.clientY, false,
-	        false, false, false, 0, null);
-
-	    touch.target.dispatchEvent(simulatedEvent);
-	    event.preventDefault();
-	}
-
-	function initTouchHandler() {
-	    document.addEventListener("touchstart", touchHandler, true);
-	    document.addEventListener("touchmove", touchHandler, true);
-	    document.addEventListener("touchend", touchHandler, true);
-	    document.addEventListener("touchcancel", touchHandler, true);
-	}
-
-
-
-
-
 	var initStartBoard = function() {
 		var startBoard = []; 
     	var player = '';
@@ -419,8 +391,6 @@ $(function() {
 // 
 // MAINLINE
 // 
-
-	initTouchHandler();
 	
  	currentBoardPlayers = initStartBoard();
  	// console.log('players',currentBoardPlayers);
